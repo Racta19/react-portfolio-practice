04c4ebb1e2872de557da8f1813484804b30427b4
siteD github <rohailomer14@gmail.com>
2022-10-24 13:14:37 +0500

    completed with dark mode

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
+//jshint esversion:6
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />; 
 }
 
-export default MyApp
+export default MyApp;
